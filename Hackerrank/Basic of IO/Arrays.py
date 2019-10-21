783db46ff9640c5a5fce29c99a453518643c318b
DEEKSHANT123 <deekshantsemwal123@gmail.com>
2019-10-21 12:49:36 +0530

    recent changes

--- Hackerrank/Basic of IO/Arrays.py
+++ Hackerrank/Basic of IO/Arrays.py
@@ -1,6 +1,5 @@
 n=int(input())
-c=0
-a=list(map(int, input().split()))
+arr=list(map(int, input().split()))
 for i in range(n):
     j=n-i-1
-    print(a[j], end=" ")
+    print(arr[j], end=" ")     #i and j define positions of elements of array.
