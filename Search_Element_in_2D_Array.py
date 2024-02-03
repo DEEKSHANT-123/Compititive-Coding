f2b07427ec838b1a9a6df2a5c3beab6b314a5922
DEEKSHANT-123 <deekshantsemwal123@gmail.com>
2024-02-03 22:54:41 +0530

    save Changes

---
+++ Search_Element_in_2D_Array.py
@@ -0,0 +1,15 @@
+from typing import List
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == target:
+                    return True
+        return False
+
+
+solution = Solution()
+matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
+target = 3
+result = solution.searchMatrix(matrix, target)
+print(result)
