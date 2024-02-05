0c712f81385955786e6fc15b1ee9f6fec6cd5eda
DEEKSHANT-123 <deekshantsemwal123@gmail.com>
2024-02-05 22:32:03 +0530

    Save changes

--- Q4Search_Element_in_2D_Array.py
+++ Q4Search_Element_in_2D_Array.py
@@ -1,3 +1,5 @@
+###########Method1 (Linear Searching in rows and columns)
+#T.C. = O(N)
 from typing import List
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
@@ -13,3 +15,7 @@ matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
 target = 3
 result = solution.searchMatrix(matrix, target)
 print(result)
+
+
+###Method 2 (Binary Search in 2D array)
+#T.C. = O(log(m*n))
